344dec9eccf22730fdbe8a4c387a26ac2b1f266a
Dmitry Kovtunov <net.izvini@yandex.ru>
2017-09-26 18:46:09 +0300

    Typescript compilation fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 const gulp = require('gulp');
+const path = require('path');
 const sourcemaps = require('gulp-sourcemaps');
 const ts = require('gulp-typescript');
 
 const tsconfig = require('./tsconfig.json');
+const dirs = {
+    APP: path.join(__dirname, 'app')
+};
 
 
 gulp.task('ts', () => {
-    console.log(tsconfig);
-    console.log(tsconfig.compilerOptions);
-    return gulp.src(['app/**/*.ts', 'app/**/*.tsx'])
-        .pipe(sourcemaps.init())
+    return gulp.src([
+        path.join(dirs.APP, '**/*.ts'),
+        path.join(dirs.APP, '**/*.tsx')
+    ]).pipe(sourcemaps.init())
         .pipe(ts(tsconfig.compilerOptions))
         .js.pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () => {
+
 });
 
 
